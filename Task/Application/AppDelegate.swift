5738c2be495d9fdc8e533bb6cae0dda5dc1f01b9
Hovo Malkhasyan <hovo.malkhasyan20@gmail.com>
2021-06-17 16:50:13 +0400

    delete Main

--- Task/Application/AppDelegate.swift
+++ Task/Application/AppDelegate.swift
@@ -13,18 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        setupWindow()
+        setupWindow()
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
     }
 }
 
